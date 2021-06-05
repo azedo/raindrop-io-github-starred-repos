0a2658aad713011c12ea08ee3ae9d5d2e17a9ae3
Eduardo Grigolo <eduardo@petmediagroup.com>
2021-06-06 01:26:47 +0200

    refactor: Improve post request
    Add an excerpt key to the item object in order to avoid the auto scrape dependency from the api that is not working. Also added more comments for better understanding.

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ import { writeFileSync, readFileSync } from 'fs';
 
 dotenv.config();
 
+// Temporary variable to store github pages
 let file
 
-async function getGithubStars(url, page = 1, previousContent = null) {
+/**
+ * First function - Get starred repos list
+ *
+ * @param {string} url
+ * @param {number} [page=1]
+ * @param {any | null} [previousContent=null]
+ */
+async function getGithubStarsList(url, page = 1, previousContent = null) {
   console.log(`Fetching page ${page}...`)
 
   const fetchUrl = `${url}?page=${page}`
@@ -26,7 +34,7 @@ async function getGithubStars(url, page = 1, previousContent = null) {
 
   if (shouldFetchAgain) {
     const nextPage = page + 1;
-    await getGithubStars('https://api.github.com/users/azedo/starred', nextPage, file);
+    await getGithubStarsList('https://api.github.com/users/azedo/starred', nextPage, file);
   } else {
     console.log(`Page ${page - 1} was the last valid page!`)
     console.log('Saving file...')
@@ -35,7 +43,10 @@ async function getGithubStars(url, page = 1, previousContent = null) {
   }
 }
 
-async function readGithubDataFile() {
+/**
+ * Second function - Parse the github list file and add the necessary fields
+ */
+async function readAndParseGithubData() {
   const dataFile = readFileSync('./data.json', { encoding: 'utf-8' })
   const parsed = JSON.parse(dataFile)
 
@@ -66,36 +77,74 @@ async function readGithubDataFile() {
       collectionId: 18455651,
       title: it.full_name,
       link: it.html_url,
-      tags: ["github", it.language],
+      tags: ["github", `${it.language}`],
       created: it.created_at,
-      pleaseParse: {}
+      excerpt: it.description
     }))
   })
   console.log(`total = ${resParsed.length}`)
   await writeFileSync('./newData.json', JSON.stringify(resParsed))
 }
 
-async function createNewRainDrop() {
+/**
+ * Create a single entry in the app
+ *
+ * @param {collectionId: number, title: string, link: string, tags: [string], created: any, description: string} item The item object
+ */
+async function createNewRainDrop(item) {
   const fetchUrl = 'https://api.raindrop.io/rest/v1/raindrop'
-  const response = await fetch(fetchUrl, {
+
+  let response = await fetch(fetchUrl, {
     method: "POST",
     headers: new Headers({
       "Authorization": `Bearer ${process.env.RAINDROP_TOKEN}`,
       "Content-Type": "application/json"
     }),
-    body: JSON.stringify({
-      collectionId: 18455651,
-      title: 'test',
-      link: 'http://www.github.com',
-      tags: ["github"],
-      pleaseParse: {}
-    })
-  }).then(res => res.json());
+    body: JSON.stringify(item)
+  })
 
-  console.log(response.result ? 'Added!' : 'Something went wrong...')
+  response = await response.json()
+  console.log(item.title, response.result ? 'Added!' : 'Something went wrong...')
 }
 
+/**
+ * Loop the array and dispatch the post requests
+ *
+ * @param {[{collectionId: number, title: string, link: string, tags: [string], created: any, description: string}]} list
+ * @param {number} [number=1]
+ */
+async function loopEntriesList(list, number = 1) {
+  console.log(`===== Block #${number} start =====`)
+  list.forEach(async entry => {
+    try {
+      await createNewRainDrop(entry)
+    } catch (error) {
+      console.log(error)
+    }
+  });
+}
+
+/**
+ * Create a multiple entries via a loop of single post requests
+ */
 async function createNewRainDrops() {
+  const dataFile = readFileSync('./newData.json', { encoding: 'utf-8' })
+  const parsed = JSON.parse(dataFile)
+
+  for (const [index, item] of parsed.entries()) {
+    const time = index === 0 ? 0 : index * 1.5
+
+    setTimeout(async () => {
+      await loopEntriesList(item, index + 1)
+    }, 60000 * time);
+  }
+}
+
+/**
+ * Create multiple entries at a time (max. supported 100 entries)
+ * Note: It doesn't do the auto fetch of the description!
+ */
+async function createNewRainDropsMultiple() {
   const fetchUrl = 'https://api.raindrop.io/rest/v1/raindrops'
   const dataFile = readFileSync('./newData.json', { encoding: 'utf-8' })
   const parsed = JSON.parse(dataFile)
@@ -114,7 +163,25 @@ async function createNewRainDrops() {
   }
 }
 
-// getGithubStars('https://api.github.com/users/azedo/starred');
-// readGithubDataFile()
-createNewRainDrops()
+// 1 - Fist run this function...
+// getGithubStarsList('https://api.github.com/users/azedo/starred');
 
+// 2 - Then run this one...
+// readAndParseGithubData()
+
+// 3 - And lastly, this one!
+// createNewRainDrops({
+//   collectionId: number,
+//   title: string,
+//   link: string,
+//   tags: [string],
+//   created: Date,
+//   excerpt: string
+// }) // Single
+// createNewRainDrops() // Multiple by single loop
+createNewRainDropsMultiple() // Multiple by 100's
+
+// 4 - Now check your raindrops.io account and all the bookmarks should be there! ;)
+// https://app.raindrop.io
+
+// Note: For API documentation for raindrop.io -> https://developer.raindrop.io/
